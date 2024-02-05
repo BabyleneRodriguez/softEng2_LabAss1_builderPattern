bd35470cb1c2a5931db8c0b006018075f0dd85ba
Babylene Rodriguez <142875371+BabyleneRodriguez@users.noreply.github.com>
2024-02-05 09:44:36 +0800

    Update eCommerce_Application.java

--- softEng2_LabAss1_builderPattern/eCommerce_Application.java
+++ softEng2_LabAss1_builderPattern/eCommerce_Application.java
@@ -5,13 +5,13 @@ public class eCommerce_Application
     public static void main(String[] args)
     {
         User user = new User.UserBuilder()
-                .firstName("Babylene")
-                .lastName("Rodriguez")
-                .email("babylene.rodriguez@neu.edu.ph")
-                .address("Taytay, Rizal")
-                .phone("09662960902")
-                .age(21)
-                .build();
+                    .firstName("Babylene")
+                    .lastName("Rodriguez")
+                    .email("babylene.rodriguez@neu.edu.ph")
+                    .address("Taytay, Rizal")
+                    .phone("09662960902")
+                    .age(21)
+                    .build();
 
         System.out.print("\n~USER ACCOUNT INFO~"
                         + "\nFirstname: " + user.firstName
